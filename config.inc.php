b83e875903b96bfd99298370ba7adaad5225f561
Harry Gabriel <h.gabriel@nodefab.de>
2012-06-15 22:32:35 +0200

    explain config options

--- config.inc.php
+++ config.inc.php
@@ -3,6 +3,8 @@
 /* Simple configutaion Array */
 $config = array(
 	'mumble_server' => 1,
+	// ice proxy connection string
 	'ice_proxy_string' => 'Meta:tcp -h 127.0.0.1 -p 6502',
+	// secret for reading from ice
 	'ice_secret_read' => ''
 	);
